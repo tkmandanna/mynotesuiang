0bb847501e5434347772ae5a972c19dd235bb973
mandanna.thekkada@gmail.com <mandanna.thekkada@gmail.com>
2024-10-15 20:29:28 -0400

    adding a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {UserNoteComponent} from './user-note/user-note.component';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserNoteComponent],
+  imports: [RouterOutlet, UserNoteComponent, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
